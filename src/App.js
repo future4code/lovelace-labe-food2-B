c2f0b19ec630912f146e1eae398fea520b235981
08-06Maisa <may.b.faustino@hotmail.com>
2021-09-08 19:07:02 +0100

    Vai dar conflito

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import theme from './constants/theme';
 // import HomePage from "./pages/Home/HomePage"
-import LoginPage from './pages/LoginPage/LoginPage';
+// import LoginPage from './pages/LoginPage/LoginPage';
 import { ThemeProvider } from "@material-ui/core"
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       {/* <HomePage/> */}
-      <LoginPage/>
+      {/* <LoginPage/> */}
+      <ProfilePage/>
     </ThemeProvider>
   );
 }
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import { AdressContainer, InfoContainer, Profile } from './styled'
+import React from "react";
+import {
+  AdressContainer,
+  InfoContainer,
+  ScreenContainer,
+} from "./styled";
 
- const ProfilePage = () => {
-    return (
-        <Profile>
-            <InfoContainer>
-                Nome: Maisa
-                E-mail: jhfuihf@gmail.com
-                CPF: 000.000.000-00
-            </InfoContainer>
-            <AdressContainer>
-                Endereço cadastrado:
-                Rua do Astronauta Perdidão, 0 - Labenu- MG
-            </AdressContainer>
-            <div>
-                Histórico de pedidos
-                {/* aqui onde vai ter os cards dos components */}
-            </div>
-        </Profile>
-    )
-}
-export default ProfilePage
+
+const ProfilePage = () => {
+  return (
+    <ScreenContainer>
+        <div>
+            Insira aqui um HEADER
+        </div>
+      <InfoContainer>
+        <p>Nome: Maisa</p>
+        <p>E-mail: jhfuihf@gmail.com</p>
+        <p>CPF: 000.000.000-00</p>
+      </InfoContainer>
+      <AdressContainer>
+        <p>Endereço cadastrado:</p>
+        <p>Rua do Astronauta Perdidão, 0 - Labenu- MG</p>
+      </AdressContainer>
+      <div>
+        Histórico de pedidos
+        {/* aqui onde vai ter os cards dos components */}
+      </div>
+    </ScreenContainer>
+  );
+};
+export default ProfilePage;
