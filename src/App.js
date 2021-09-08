d5313f5e9f7af0f55f6434fefdbcd2b4d2522321
GuilhermeMedici <mailguigs@gmail.com>
2021-09-08 12:09:02 -0300

    finalizada a página de cadastro informações pessoais

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import  HomePage  from './pages/Home/HomePage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
 
 function App() {
   return (
     <div>
-    <HomePage/>
+    {/* <HomePage/> */}
+    <SignUpPage/>
     </div>
-  );
+  )
 }
 
 export default App;
