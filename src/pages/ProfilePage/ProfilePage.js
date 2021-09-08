fc9d241418a435becddeca2035f30e0682db3dff
08-06Maisa <may.b.faustino@hotmail.com>
2021-09-08 15:01:33 +0100

    Fiz o styled básico do ProfilePage

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { AdressContainer, InfoContainer, Profile } from './styled'
 
  const ProfilePage = () => {
     return (
-        <div>
-            
-        </div>
+        <Profile>
+            <InfoContainer>
+                Nome: Maisa
+                E-mail: jhfuihf@gmail.com
+                CPF: 000.000.000-00
+            </InfoContainer>
+            <AdressContainer>
+                Endereço cadastrado:
+                Rua do Astronauta Perdidão, 0 - Labenu- MG
+            </AdressContainer>
+            <div>
+                Histórico de pedidos
+                {/* aqui onde vai ter os cards dos components */}
+            </div>
+        </Profile>
     )
 }
 export default ProfilePage
